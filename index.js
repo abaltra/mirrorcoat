367b38d73b43f0dc810d1f87f8487cda105d0f3f
Alejandro Baltra <abaltra@gmail.com>
2018-10-01 00:38:49 -0300

    Added start script. Parametrized port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 3000
 
 app.use("/api/:code", (req, res, end) => {
     console.log("hai")
@@ -9,6 +10,6 @@ app.use("/api/:code", (req, res, end) => {
     res.send()
 })
 
-app.listen(3000, () => {
-    console.log("App started")
+app.listen(port, () => {
+    console.log("App started in port " + port)
 })
